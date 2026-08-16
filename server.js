d17e9efaac07176674e849f67e97fc5f775db5df
Samuel <victorsamuel0909@gmail.com>
2026-08-16 13:08:15 -0300

    Adiciona CORS para integração com WordPress

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ import { httpServerHandler } from "cloudflare:node";
 import express from "express";
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("API funcionando no Cloudflare!");
